99cf4bc181e181e35071da6e7eff934468de021a
JuliaRinzilotti <91470769+JuliaRinzilotti@users.noreply.github.com>
2022-12-07 15:48:03 +0100

    erste Komponente

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <p>LOGO</p>
+            <p>About</p>
+            <p>Contact</p>
+        </nav>
+    )
+}
+
+export default Navbar
